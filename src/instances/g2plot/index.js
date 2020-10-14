54f4428a1a33bdf85cc823d4acdcfdb9d2138543
e <1271255653@qq.com>
2020-10-14 17:05:20 +0800

    feat: :triangular_flag_on_post: 添加G2Plot图表配置

--- src/instances/g2plot/index.js
+++ src/instances/g2plot/index.js
@@ -1,1 +1,18 @@
 // read /doc/G2PLOT.md
+import * as g2plot from '@antv/g2plot'
+
+class G2Plot {
+  constructor (chartType, elements, options) {
+    this.chartType = chartType
+    this.elements = elements
+    this.options = options
+
+    this.exportG2Plot()
+  }
+
+  exportG2Plot () {
+    return new g2plot[this.chartType](this.elements, this.options)
+  }
+}
+
+export { G2Plot }
