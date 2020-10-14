f43da36ecc7bbab4dc27ff5c9494f5c9b145dbcf
e <1271255653@qq.com>
2020-10-14 18:29:48 +0800

    test: :white_check_mark: 测试

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-// import isSupportedBrowser from './helpers/is-supported-browser'
-// if (!isSupportedBrowser()) throw Error('需支持IE8以上、以及Mapbox所支持的浏览器)')
+import isSupportedBrowser from './helpers/is-supported-browser'
+if (!isSupportedBrowser()) throw Error('需支持IE8以上、以及Mapbox所支持的浏览器)')
 
-// // custom chart
-// export { Geoc } from './instances/geoc'
+// custom chart
+export { Geoc } from './instances/geoc'
 
-// // G2Plot
-// export { G2Plot } from './instances/g2plot'
+// G2Plot
+export { G2Plot } from './instances/g2plot'
 
-// // G2
-// export { G2 } from './instances/g2'
+// G2
+export { G2 } from './instances/g2'
 
-// // Mapbox
-// export { Mapbox } from './instances/mapbox'
+// Mapbox
+export { Mapbox } from './instances/mapbox'
 
-// // ...
-console.log(11111111)
+// ...
