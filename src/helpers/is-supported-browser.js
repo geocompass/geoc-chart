1677665fb71a8b61622de4a8e9ebfbfad1129c0a
e <1271255653@qq.com>
2020-10-14 18:27:42 +0800

    test: :white_check_mark: 测试

--- src/helpers/is-supported-browser.js
+++ src/helpers/is-supported-browser.js
@@ -1,16 +1,11 @@
-import mapboxgl from '../static/mapbox-gl'
-
 // 是否支持运行浏览器 false不支持 true支持
 const isSupportedBrowser  = () => {
   const BrowserInfo = navigator.userAgent.toLowerCase()
   let browserVersion
 
-  // <IE8
+  // <IE11
   if (BrowserInfo.indexOf('msie') > -1) browserVersion = BrowserInfo.match(/msie ([\d.]+)/)[1]
   if (browserVersion && browserVersion <= 10.0) return false
-
-  // mapbox supported
-  if (!mapboxgl.supported()) return false
 
   return true
 }
--- src/instances/mapbox/index.js
+++ src/instances/mapbox/index.js
@@ -5,35 +5,14 @@ class Mapbox {
   constructor (elements, options) {
     const {
       mapStyle,
-      mapTiles,
       mapCenter = [122.48356819152832, 37.832056363179625],
       mapZoom = 4
     } = options
 
     this._style = mapStyle
-    this._tiles = mapTiles
     this._center = mapCenter
     this._zoom = mapZoom
     this.elements = elements
-  }
-
-  // 添加底图样式
-  addMapStyle (mapStyle, mapTiles) {
-    if (!mapStyle.sources) mapStyle.sources = {}
-
-    mapStyle.sources.gde_source = {
-      'type': 'vector',
-      'tiles': mapTiles
-    }
-    for (let layer of mapStyle.layers) {
-      if (layer.type === 'background') continue
-      if (!layer.source) {
-        layer.source = 'gde_source'
-        layer['source-layer'] = 'gde_layer'
-      }
-    }
-
-    this._map.setStyle(mapStyle)
   }
 
   // 图表渲染
@@ -44,8 +23,6 @@ class Mapbox {
       center: this._center,
       zoom: this._zoom
     })
-
-    this.addMapStyle(this._style, this._tiles)
   }
 
   // 图表销毁
